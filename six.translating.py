49e180a897e31c603e9509eb5fc1c1ab6d4ce841
Lubov Kostikova <lu2005ko@gmail.com>
2025-09-21 21:33:27 +0300

    Create six.translating.py

---
+++ six.translating.py
@@ -0,0 +1,81 @@
+table_of_codon = {
+    "UUU": "F",
+    "UUC": "F",
+    "UUA": "L",
+    "UUG": "L",
+    "CUU": "L",
+    "CUC": "L",
+    "CUA": "L",
+    "CUG": "L",
+    "AUU": "I",
+    "AUC": "I",
+    "AUA": "I",
+    "AUG": "M",
+    "GUU": "V",
+    "GUC": "V",
+    "GUA": "V",
+    "GUG": "V",
+    "UCU": "S",
+    "UCC": "S",
+    "UCA": "S",
+    "UCG": "S",
+    "CCU": "P",
+    "CCC": "P",
+    "CCA": "P",
+    "CCG": "P",
+    "ACU": "T",
+    "ACC": "T",
+    "ACA": "T",
+    "ACG": "T",
+    "GCU": "A",
+    "GCC": "A",
+    "GCA": "A",
+    "GCG": "A",
+    "UAU": "Y",
+    "UAC": "Y",
+    "UAA": "stop",
+    "UAG": "stop",
+    "CAU": "H",
+    "CAC": "H",
+    "CAA": "Q",
+    "CAG": "Q",
+    "AAU": "N",
+    "AAC": "N",
+    "AAA": "K",
+    "AAG": "K",
+    "GAU": "D",
+    "GAC": "D",
+    "GAA": "E",
+    "GAG": "E",
+    "UGU": "C",
+    "UGC": "C",
+    "UGA": "stop",
+    "UGG": "W",
+    "CGU": "R",
+    "CGC": "R",
+    "CGA": "R",
+    "CGG": "R",
+    "AGU": "S",
+    "AGC": "S",
+    "AGA": "R",
+    "AGG": "R",
+    "GGU": "G",
+    "GGC": "G",
+    "GGA": "G",
+    "GGG": "G",
+}
+
+with open('hw2/rna.txt') as file:
+    rna_sequence = file.readline()
+
+protein_breakdown = ""
+for i in range(0, len(rna_sequence), 3):
+    rna_codon = rna_sequence[i] + rna_sequence[i+1] + rna_sequence[i+2]
+    aacid = table_of_codon.get(rna_codon)
+    if aacid == "stop":
+        break
+    protein_breakdown += aacid
+
+with open("hw2/rna.txt", "a") as file:
+    file.write('\n' + "Белок по трансляции РНК:" + '\n')
+    file.write(f'{protein_breakdown}')
